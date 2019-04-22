109dc3c94a6a64fb0e83099714b528e58e150fed
swethapremkumar <49434762+swethapremkumar@users.noreply.github.com>
2019-04-21 22:14:51 -0700

    Add files via upload

---
+++ teamroles.php
@@ -0,0 +1,68 @@
+<html>
+<title>TEAM ROLES</title>
+<body>
+<input type="button" value="Add" id="add"><br><br>
+<input type="button" value="Remove" id="rem"><br><br>
+<style>
+table,th,td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+th,td{
+	padding: 15px;
+}
+table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+table{
+	border-spacing: 15px;
+}
+</style>
+
+<table style="width:100%">
+  <tr>
+	<td>S.NO</td>
+    <td>USER ID</td>
+    <td>NAME</td>
+    <td>ROLES</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+</table>
+</body>
+</html>
+
